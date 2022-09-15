5f313f6545466f02dd9b40a0f325e42428e72fbb
Alex Berry <alexberry83@gmail.com>
2022-09-15 17:22:37 +0100

    Update Controller.java
    put endpoint

--- src/main/java/com/integration/controller/Controller.java
+++ src/main/java/com/integration/controller/Controller.java
@@ -1,16 +1,18 @@
 package com.integration.controller;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,7 +52,6 @@ public class Controller {
 	@ResponseStatus(code = HttpStatus.ACCEPTED)
 	@ResponseBody
 	public JournalH2Datamodel postJournalContent(@RequestBody JournalH2Datamodel journal) {
-		//DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		String id = UUID.randomUUID().toString();
 		journal.setId(id);
 		journal.setStatus("To Process");
@@ -64,5 +65,36 @@ public class Controller {
 			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to upload content at this time");
 		}
 		
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseBody
+	public JournalH2Datamodel updateJournal(@PathVariable String id, @RequestParam String paramType, @RequestBody JournalH2Datamodel journal) {
+		if(!paramType.equalsIgnoreCase("message")) {
+			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Param specified to update. Please supply query param as ?paramTyne= and try again");
+		} else {
+			Optional<JournalH2Datamodel> optionalJournal = this.journalRepo.findById(id);
+			if(optionalJournal.isPresent()) {
+				JournalH2Datamodel journalToUpdate = optionalJournal.get();
+				journalToUpdate.setMessage(journal.getMessage());
+				this.journalRepo.save(journalToUpdate);
+				return this.journalRepo.findById(id).get();
+			} else {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Journal found with supplied id. Please try again with different id");
+			}
+		}
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	@ResponseBody
+	public JournalH2Datamodel deleteJournal(@PathVariable("id") String id) {
+		Optional<JournalH2Datamodel> optionalJournalToDelete = this.journalRepo.findById(id);
+		if(optionalJournalToDelete.isPresent()) {
+			this.journalRepo.delete(optionalJournalToDelete.get());
+			return optionalJournalToDelete.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Journal found with supplied id. Please try again with different id");
+		}
 	}
 }
